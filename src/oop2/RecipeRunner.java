424e50e213decf041ef8c58bc725d77db30c4633
rowoon <dev.rowoon@gmail.com>
2023-08-28 17:18:29 +0900

    [add] 전자레인지 클래스 추가

--- src/oop2/RecipeRunner.java
+++ src/oop2/RecipeRunner.java
@@ -4,5 +4,8 @@ public class RecipeRunner {
     public static void main(String[] args) {
         Recipe1 recipe1 = new Recipe1();
         recipe1.execute();
+
+        RecipeWithMicrowave recipeWithMicrowave = new RecipeWithMicrowave();
+        recipeWithMicrowave.execute();
     }
 }
