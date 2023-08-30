906e87d51da498315411a95f06397c37fccdc03b
rowoon <dev.rowoon@gmail.com>
2023-08-30 19:28:38 +0900

    [mod] git 교육용 커밋

--- src/learnInterface/GameRunner.java
+++ src/learnInterface/GameRunner.java
@@ -2,7 +2,7 @@ package learnInterface;
 
 public class GameRunner {
     public static void main(String[] args) {
-        // 콘솔에 어떤 칩을 꽂냐에 따라서 결과가 달라짐(다형성)
+        // 콘솔에 어떤 칩을 꽂냐에 따라서 결과가 달라짐
         GamingConsole marioGame = new MarioGame();
         GamingConsole chessGame = new ChessGame();
 
